fdc46b2829af2ce8a000e6348e0a85b77d6bbc12
MargauxBrandon <148874302+MargauxBrandon@users.noreply.github.com>
2024-08-27 12:12:02 +0200

    Update test_read.py

--- tests/test_read.py
+++ tests/test_read.py
@@ -1,10 +1,11 @@
 import optode
 
 filenames = [
-    "data/underway_pH_1.txt",
-    "data/2020-12-11_163148_NAPTRAM2020/2020-12-11_163148_NAPTRAM2020.txt",
-    "data/2022-02-24_221145_SO289/2022-02-24_221145_SO289.txt",
-    "data/St16_VIDEO1_23430014.txt",
+    # "data/underway_pH_1.txt",
+    "data/2023-05-26_145716_64PE517_NoSE_part3/2023-05-26_145716_64PE517_NoSE_part3.txt"
+    # "data/2020-12-11_163148_NAPTRAM2020/2020-12-11_163148_NAPTRAM2020.txt",
+    # "data/2022-02-24_221145_SO289/2022-02-24_221145_SO289.txt",
+    # "data/St16_VIDEO1_23430014.txt",
 ]
 
 for filename in filenames:

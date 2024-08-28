051c95aff16b13d5a70d4534dd34bdc10c33dede
Louise Delaigue <louisedelaigue@gmail.com>
2024-08-28 13:46:23 +0200

    added function to read all txt files in a folder

--- tests/test_read_file.py
+++ tests/test_read_file.py
@@ -4,9 +4,9 @@ filenames = [
     # "data/underway_pH_1.txt",
     # "data/2023-05-26_145716_64PE517_NoSE_part3/2023-05-26_145716_64PE517_NoSE_part3.txt",
     # "data/2020-12-11_163148_NAPTRAM2020/2020-12-11_163148_NAPTRAM2020.txt",
-    # "data/2022-02-24_221145_SO289/2022-02-24_221145_SO289.txt",
-    "data/St16_VIDEO1_23430014.txt",
+    "data/2022-02-24_221145_SO289/2022-02-24_221145_SO289.txt",
+    # "data/St16_VIDEO1_23430014.txt",
 ]
 
 for filename in filenames:
-    data = optopHyre.read_split.read_pyrosci(filename)
+    data = optopHyre.read.file(filename)
